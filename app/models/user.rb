fb241b646033f1eb0b1f9e40b2aa75af30c4ff97
Manezeu Patricia <pmanezeu@gmail.com>
2020-05-27 16:08:36 +0100

    Finished the user's moderl tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
-    validates_uniqueness_of :username
     has_many :created_events, class_name: 'Event', foreign_key: 'creator_id'
     has_many :attendances, foreign_key: 'attendee_id'
     has_many :attended_events, through: :attendances, source: :attended_event
 
     has_many :invitations, foreign_key: 'guest_id'
     has_many :invited_events, through: :invitations, source: :invited_event
+
+
+    validates_uniqueness_of :username
     
     def pending_events?(event)
         invited_events.include?(event) &&
